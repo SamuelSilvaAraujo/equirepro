269efcf8b8f7c68b0b2602f77456a5a1766bfaf2
samuel <samuelsas.samuel18@gmail.com>
2019-01-28 20:15:54 -0300

    add widgets in form login

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,7 +1,28 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import PasswordInput, TextInput
+
 from authentication.models import User
+from django import forms
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+
 
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['email', 'crmv', 'name']
+        fields = ['email', 'crmv', 'name',]
+        widgets = {
+            'email': forms.TextInput(attrs={'placeholder': 'E-mail', 'class': 'form-control'}),
+            'crmv': forms.TextInput(attrs={'placeholder': 'CRMV', 'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'placeholder': 'Nome', 'class': 'form-control'}),
+
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Senha'})
+        self.fields['password2'].widget = PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Confirme sua Senha'})
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,11 @@
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from authentication.forms import RegisterForm
+from authentication.forms import RegisterForm, LoginForm
 
 class Login(LoginView):
     template_name = 'login.html'
+    form_class = LoginForm
 
 class Register(CreateView):
     template_name = 'register.html'
