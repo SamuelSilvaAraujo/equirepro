de401850762d823e5e7a1bed62e4e23a7f515a58
samuels <samuelsas.samuel18@gmail.com>
2019-01-30 12:20:54 -0300

    add pages auxiliares, clientes e haras_list e colocando calendar em portugues

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from core.views import Agenda, Animais, NovoAnimal
+from core.views import *
 
 urlpatterns = [
     path('agenda/', Agenda.as_view(), name="agenda"),
     path('animal/list/', Animais.as_view(), name="animal_list"),
     path('animal/novo/', NovoAnimal.as_view(), name="animal_novo"),
+    path('cliente/list/', Clientes.as_view(), name="client_list"),
+    path('haras/list/', HarasList.as_view(), name="haras_list"),
+    path('auxiliar/list/', Auxiliares.as_view(), name="auxiliar_list"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from core.models import Animal
+from core.models import *
 from core.forms import AnimalForm
 
 class Home(LoginRequiredMixin, TemplateView):
@@ -11,6 +11,11 @@ class Home(LoginRequiredMixin, TemplateView):
 
 class Agenda(LoginRequiredMixin, TemplateView):
     template_name = 'agenda.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Agenda, self).get_context_data(**kwargs)
+        context["agenda_page"] = "active"
+        return context
 
 class Animais(LoginRequiredMixin, ListView):
     model = Animal
@@ -21,6 +26,7 @@ class Animais(LoginRequiredMixin, ListView):
         context["garanhoes"] = self.request.user.animal_set.filter(type="GANHARAO")
         context["doadoras"] = self.request.user.animal_set.filter(type="DOADORA")
         context["receptoras"] = self.request.user.animal_set.filter(type="RECEPTORA")
+        context["animal_page"] = "active"
         return context
 
 class NovoAnimal(LoginRequiredMixin, CreateView):
@@ -33,3 +39,30 @@ class NovoAnimal(LoginRequiredMixin, CreateView):
         obj.user = self.request.user
         obj.save()
         return HttpResponseRedirect(reverse_lazy('animal_list'))
+
+class Clientes(LoginRequiredMixin, ListView):
+    model = Client
+    template_name = 'clientes.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Clientes, self).get_context_data(**kwargs)
+        context["client_page"] = "active"
+        return context
+
+class HarasList(LoginRequiredMixin, ListView):
+    model = Haras
+    template_name = 'haras_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HarasList, self).get_context_data(**kwargs)
+        context["haras_page"] = "active"
+        return context
+
+class Auxiliares(LoginRequiredMixin, ListView):
+    model = Ancillary
+    template_name = 'auxiliares.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Auxiliares, self).get_context_data(**kwargs)
+        context["auxiliar_page"] = "active"
+        return context
